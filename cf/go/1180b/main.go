4fd67f343b2e6094d624a9963e0cb1a729aa228a
huxulm <huxulm@gmail.com>
2023-10-31 10:29:33 +0800

    add 1180B

---
+++ cf/go/1180b/main.go
@@ -0,0 +1,33 @@
+package main
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "io"
+)
+func solve(_r io.Reader, _w io.Writer) {
+    in := bufio.NewReader(_r)
+    out := bufio.NewWriter(_w)
+    defer out.Flush()
+    var n, t int
+    fmt.Fscan(in, &n)
+	a := make([]int, n)
+	for i := range a {
+		fmt.Fscan(in, &a[i])
+		if a[i] >= 0 {xc 
+			a[i] = -a[i]-1
+		}
+		if a[i] < a[t] {
+			t = i
+		}
+	}
+    if n % 2 > 0 {
+		a[t] = -a[t] - 1
+	}
+    for _, v := range a {
+        fmt.Fprint(out, v, " ")
+    }
+}
+func main() {
+    solve(os.Stdin, os.Stdout)
+}
